57febd6cbc9ead12a20784e3597c578f9099a9bb
csmithsalzberg <caleb.smithsalzberg@gmail.com>
2017-11-09 21:37:03 -0500

    done

--- app/app.py
+++ app/app.py
@@ -1,16 +1,27 @@
+#Caleb Smith-Salzberg
+#SoftDev1 pd7
+#HW 13 -- A RESTful Journey Skyward
+#2017-11-09
 import urllib2
+import json
 from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-url = "https://api.nasa.gov/planetary/apod?api_key=JgfRHHASa1UjyzXkVEgwEBboEhjVH8QGAQVhA4cb"
 
-uResp = urllib2.urlopen( url ) 
-contentsraw = uResp.read()
-print uResp.info()
 
+@app.route("/")
+def apifun():
+    url = "https://api.nasa.gov/planetary/apod?api_key=JgfRHHASa1UjyzXkVEgwEBboEhjVH8QGAQVhA4cb"
+    uResp = urllib2.urlopen( url ) 
+    contentsraw = uResp.read()
+    dat = json.loads(contentsraw)
+    image = dat["url"]
+    description = dat["explanation"]
+    return render_template("display.html", img = image, desc = description)
 
 if __name__ == '__main__':
     app.debug = True
     app.run()
+apifun()
