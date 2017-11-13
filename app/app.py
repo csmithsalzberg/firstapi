68a865cb3c135bb85f21457b4740aa62adf37444
csmithsalzberg <caleb.smithsalzberg@gmail.com>
2017-11-12 23:48:06 -0500

    to pull

--- app/app.py
+++ app/app.py
@@ -19,7 +19,13 @@ def apifun():
     dat = json.loads(contentsraw)
     image = dat["url"]
     description = dat["explanation"]
-    return render_template("display.html", img = image, desc = description)
+
+    url2 = "https://api.weather.gov/points/40.7179,-74.0135/forecast"
+    uResp2 = urllib2.urlopen( url2)
+    contentsraw2 = uResp2.read()
+    dat2 = json.loads(contentsraw)
+    weather = dat2["@context"]
+    return render_template("display.html", img = image, desc = description, weather = forecast)
 
 if __name__ == '__main__':
     app.debug = True
