f27821358df124f102823ae6dd5198afe8da8b5b
csmithsalzberg <caleb.smithsalzberg@gmail.com>
2017-11-13 00:41:53 -0500

    donee

--- app/app.py
+++ app/app.py
@@ -20,13 +20,12 @@ def apifun():
     image = dat["url"]
     description = dat["explanation"]
 
-    url2 = "https://api.weather.gov/points/40.7179,-74.0135/forecast"
-    uResp2 = urllib2.urlopen( url2)
+    temp = urllib2.Request("https://api.weather.gov/points/40.7179,-74.0135/forecast", headers={ 'Accept' : '*/*'})
+    uResp2 = urllib2.urlopen( temp )
     contentsraw2 = uResp2.read()
-    dat2 = json.loads(contentsraw)
-    weather = dat2["@context"]
+    dat2 = json.loads(contentsraw2)
+    forecast = "Weather at Stuyvesant High School: \n\n" + dat2["properties"]["periods"][0]["detailedForecast"] + "\n\n\n"
     return render_template("display.html", img = image, desc = description, weather = forecast)
-
 if __name__ == '__main__':
     app.debug = True
     app.run()
